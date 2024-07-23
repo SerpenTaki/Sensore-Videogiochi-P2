9c6e6e63715d1937b65f020dd718fd5d1226bfb8
SerpenTaki <gabrielecorubolo@gmail.com>
2024-07-23 17:44:53 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,39 @@
 #include<QApplication>
 #include<QLabel>
+#include<QVBoxLayout>
+#include<QPushButton>
+#include<QHBoxLayout>
+
+class test: public QWidget{
+private:
+  QVBoxLayout* nuovo;
+  QHBoxLayout* nuovo2;
+  QHBoxLayout* grande;
+public:
+  test(QWidget* parent = nullptr): QWidget(parent){
+    nuovo = new QVBoxLayout;
+    QLabel* n = new QLabel("Nus");
+    nuovo->addWidget(n); 
+    QPushButton* bottone = new QPushButton("clicca qui");
+    nuovo->addWidget(bottone);
+    connect(bottone, &QPushButton::clicked, this /*classe dove si trova lo slot*/, &test::stampaN);
+    nuovo2 = new QHBoxLayout;
+    QLabel* negroni = new QLabel("Voglio Bere");
+    nuovo2->addWidget(negroni);
+    grande = new QHBoxLayout(this);
+    grande->addLayout(nuovo);
+    grande->addLayout(nuovo2);
+  }
+public slots:
+  void stampaN() {
+    QLabel* testo = new QLabel("ciao");
+    nuovo->addWidget(testo);
+  }
+};
 
 int main(int argv, char** argc){
   QApplication SensoreGioco(argv, argc);
-  QLabel* finestra = new QLabel("Sensore Danno");
-  finestra->show();
+  test t;
+  t.show();
   return SensoreGioco.exec();
 }
