87bee47138900ef88901838d95611e34f2ee3c43
SerpenTaki <gabrielecorubolo@gmail.com>
2024-07-24 23:03:20 +0200

    separazione per rightsidebar

--- QT_files/headers/rightSideBar.h
+++ QT_files/headers/rightSideBar.h
@@ -17,27 +17,9 @@ private:
     QPushButton* eliminaSensButt;
     content* cont;
 public:
-    rightSideBar(content* c,QWidget* parent = nullptr): QWidget(parent), cont(c){
-        right = new QVBoxLayout(this);
-        avviaSimulaButt = new QPushButton("Avvia Simulazione");
-        modificaSensButt = new QPushButton("Modifica Sensore");
-        eliminaSensButt = new QPushButton("Elimina Sensore");
-        right->addWidget(avviaSimulaButt);
-        right->addWidget(modificaSensButt);
-        right->addWidget(eliminaSensButt);
-
-        if (cont) {
-            connect(avviaSimulaButt, &QPushButton::clicked, cont, &content::avviaSimulazione);
-            connect(eliminaSensButt, &QPushButton::clicked, cont, &content::eliminaSensore);
-        }
-        
-        connect(modificaSensButt, &QPushButton::clicked, this, &rightSideBar::modificaSensore);
-    }
+    rightSideBar(content* c,QWidget* parent = nullptr);
 public slots:
-    void modificaSensore(){
-        QLabel* testoModifica = new QLabel("Modifica Sensore");
-        testoModifica->show();
-    }
+    void modificaSensore();
 };
 
 #endif
