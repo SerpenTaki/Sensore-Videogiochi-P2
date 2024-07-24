410ed8c4434ae235211218f5369a958e01283a43
SerpenTaki <gabrielecorubolo@gmail.com>
2024-07-24 22:55:27 +0200

    separata leftSideBar

--- QT_files/headers/leftSideBar.h
+++ QT_files/headers/leftSideBar.h
@@ -14,28 +14,10 @@ private:
     QPushButton* aggiungiSensoreBottone;
     QLineEdit* searchBox;
 public:
-    leftSideBar(QWidget* parent = nullptr): QWidget(parent){
-        left = new QVBoxLayout(this);
-        aggiungiSensoreBottone = new QPushButton("Aggiungi Sensore");
-        searchBox = new QLineEdit();
-        searchBox->setPlaceholderText("Cerca sensore..."); // Placeholder per la search box
-        left->addWidget(searchBox);
-        left->addWidget(aggiungiSensoreBottone);
-        connect(aggiungiSensoreBottone, &QPushButton::clicked, this, &leftSideBar::stampaSelSensore);
-        connect(searchBox, &QLineEdit::textChanged, this, &leftSideBar::eseguiRicerca);
-    }
-
+    leftSideBar(QWidget* parent = nullptr);
 public slots:
-    void stampaSelSensore(){
-        QLabel* testo = new QLabel("Specifica il tipo di sensore");
-        testo->show();
-    }
-
-    void eseguiRicerca(const QString& text){ //DA FARE
-        // Logica di ricerca qui
-        // Ad esempio, puoi filtrare una lista di sensori o aggiornare un display basato sul testo
-        qDebug() << "Ricerca: " << text;
-    }
+    void stampaSelSensore();
+    void eseguiRicerca(const QString& text);
 };
 
 
---
+++ QT_files/leftSideBar.cpp
@@ -0,0 +1,25 @@
+#include "headers/leftSideBar.h"
+
+leftSideBar::leftSideBar(QWidget* parent = nullptr)
+ : QWidget(parent)
+{
+    left = new QVBoxLayout(this);
+    aggiungiSensoreBottone = new QPushButton("Aggiungi Sensore");
+    searchBox = new QLineEdit();
+    searchBox->setPlaceholderText("Cerca sensore..."); // Placeholder per la search box
+    left->addWidget(searchBox);
+    left->addWidget(aggiungiSensoreBottone);
+    connect(aggiungiSensoreBottone, &QPushButton::clicked, this, &leftSideBar::stampaSelSensore);
+    connect(searchBox, &QLineEdit::textChanged, this, &leftSideBar::eseguiRicerca);
+}
+
+void leftSideBar::stampaSelSensore(){
+    QLabel* testo = new QLabel("Specifica il tipo di sensore");
+    testo->show();
+}
+
+void leftSideBar::eseguiRicerca(const QString& text){ //DA FARE
+    // Logica di ricerca qui
+    // Ad esempio, puoi filtrare una lista di sensori o aggiornare un display basato sul testo
+    qDebug() << "Ricerca: " << text;
+}
