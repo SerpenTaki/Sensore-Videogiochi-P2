d61b2e81a424558498da8bb3a4f85f25fae6fecb
SerpenTaki <gabrielecorubolo@gmail.com>
2024-07-30 14:44:31 +0200

    implementanzione di content

---
+++ QT_files/content.cpp
@@ -0,0 +1,26 @@
+#include"headers/content.h"
+
+content::content(QWidget* parent)
+: QWidget(parent){
+    center = new QVBoxLayout(this);
+    grafichino = new QLabel("Mostra qui il grafico");
+    center->addWidget(grafichino);
+}
+
+void content::avviaSimulazione(){
+    QLabel* sim = new QLabel("Simulazione Avviata");
+    center->addWidget(sim);
+}
+
+void content::eliminaSensore(){ // Da sistemare
+       /* QLayoutItem* item;
+        while ((item = center->takeAt(0))) {
+        QWidget* widget = item->widget();
+        if (widget) {
+            widget->deleteLater(); // Rimuove e pianifica la cancellazione del widget
+        }
+        delete item; // Rimuove l'elemento di layout
+        QLabel* messaggioRimozione = new QLabel("Sensore Rimosso");
+        center->addWidget(messaggioRimozione);
+        }*/ //Buggata da sistemare
+}
--- QT_files/headers/content.h
+++ QT_files/headers/content.h
@@ -11,29 +11,11 @@ private:
     QVBoxLayout* center;
     QLabel* grafichino;
 public:
-    content(QWidget* parent = nullptr): QWidget(parent){
-        center = new QVBoxLayout(this);
-        grafichino = new QLabel("Mostra qui il grafico");
-        center->addWidget(grafichino);
-    }
+    content(QWidget* parent = nullptr);
 public slots:
-    void avviaSimulazione(){
-        QLabel* sim = new QLabel("Simulazione Avviata");
-        center->addWidget(sim);
-    }
+    void avviaSimulazione();
 
-    void eliminaSensore(){ // Da sistemare
-       /* QLayoutItem* item;
-        while ((item = center->takeAt(0))) {
-        QWidget* widget = item->widget();
-        if (widget) {
-            widget->deleteLater(); // Rimuove e pianifica la cancellazione del widget
-        }
-        delete item; // Rimuove l'elemento di layout
-        QLabel* messaggioRimozione = new QLabel("Sensore Rimosso");
-        center->addWidget(messaggioRimozione);
-        }*/ //Buggata da sistemare
-    }
+    void eliminaSensore();
 };
 
 
