7b7d020b3f5c7771dea88c5a6dcd669a788dd260
SerpenTaki <gabrielecorubolo@gmail.com>
2024-08-28 16:09:08 +0200

    migliorie

--- core_files/contentvisitor.cpp
+++ core_files/contentvisitor.cpp
@@ -7,38 +7,29 @@ ContentVisitor::~ContentVisitor() {
 }
 
 void ContentVisitor::visitFisico(fisico* f){
-    dispWidget = new QLabel("Sensore Fisico \nNome:\t\t\t" + QString::fromStdString(f->getNome()) +
-     "\nDannoBase:\t\t" + QString::number(f->getDanno()) +
-     "\nNumero di Turni:\t\t" + QString::number(f->getNTurni()) +
+    dispWidget = new QLabel("Sensore Fisico \nNome:\t\t\t" + QString::fromStdString(f->getNome()) + "\tNumero di Hit:\t\t" + QString::number(f->getNHit()) +
+     "\nDannoBase:\t\t" + QString::number(f->getDanno()) + "\tNumero di Miss:\t\t" + QString::number(f->getNMiss()) +
+     "\nNumero di Turni:\t\t" + QString::number(f->getNTurni()) + "\tHit Rate:\t\t\t" + QString::number(f->getHitRate()) + "%"+
      "\nAttacchi per Turno:\t" + QString::number(f->getAtt()) + 
-     "\nAffilatura:\t\t" + QString::number(f->getAffilatura()) + "%" +
-     "\nNumero di Hit:\t\t" + QString::number(f->getNHit()) +
-     "\nNumero di Miss:\t\t" + QString::number(f->getNMiss()) +
-     "\nHit Rate:\t\t\t" + QString::number(f->getHitRate()) + "%");
+     "\nAffilatura:\t\t" + QString::number(f->getAffilatura()) + "%");
 }
 
 void ContentVisitor::visitMagico(magico* m){
-    dispWidget = new QLabel("Sensore Magico \nNome:\t\t\t" + QString::fromStdString(m->getNome()) +
-     "\nDannoBase:\t\t" + QString::number(m->getDanno()) +
-     "\nNumero di Turni:\t\t" + QString::number(m->getNTurni()) +
+    dispWidget = new QLabel("Sensore Magico \nNome:\t\t\t" + QString::fromStdString(m->getNome()) + "\tNumero di Hit:\t\t" + QString::number(m->getNHit()) +
+     "\nDannoBase:\t\t" + QString::number(m->getDanno()) + "\tNumero di Miss:\t\t" + QString::number(m->getNMiss()) +
+     "\nNumero di Turni:\t\t" + QString::number(m->getNTurni()) + "\tHit Rate:\t\t\t" + QString::number(m->getHitRate()) + "%" +
      "\nAttacchi per Turno:\t" + QString::number(m->getAtt()) + 
      "\nLivello Magia:\t\t" + QString::number(m->getLvMagia()) +
-     "\nCondizione:\t\t" + (m->checkCondition() ? "Si" : "No") +
-     "\nNumero di Hit:\t\t" + QString::number(m->getNHit()) +
-     "\nNumero di Miss:\t\t" + QString::number(m->getNMiss()) +
-     "\nHit Rate:\t\t\t" + QString::number(m->getHitRate()) + "%");
+     "\nCondizione:\t\t" + (m->checkCondition() ? "Si" : "No"));
 }
 
 void ContentVisitor::visitSacro(sacro* s){
     dispWidget = new QLabel("Sensore Sacro \nNome:\t\t\t" + QString::fromStdString(s->getNome()) +
-     "\nDannoBase:\t\t" + QString::number(s->getDanno()) +
-     "\nNumero di Turni:\t\t" + QString::number(s->getNTurni()) +
-     "\nAttacchi per Turno:\t" + QString::number(s->getAtt()) + 
+     "\nDannoBase:\t\t" + QString::number(s->getDanno()) + "\tNumero di Hit:\t\t" + QString::number(s->getNHit()) +
+     "\nNumero di Turni:\t\t" + QString::number(s->getNTurni()) + "\tNumero di Miss:\t\t" + QString::number(s->getNMiss()) +
+     "\nAttacchi per Turno:\t" + QString::number(s->getAtt()) + "\tHit Rate:\t\t\t" + QString::number(s->getHitRate()) +
      "\nLivello Fede:\t\t" + QString::number(s->getLvFede()) +
-     "\nValore Limit:\t\t" + QString::number(s->getlimitBreak()) +
-     "\nNumero di Hit:\t\t" + QString::number(s->getNHit()) +
-     "\nNumero di Miss:\t\t" + QString::number(s->getNMiss()) +
-     "\nHit Rate:\t\t\t" + QString::number(s->getHitRate()) + "%");
+     "\nValore Limit:\t\t" + QString::number(s->getlimitBreak()));
 }
 
 QWidget* ContentVisitor::returnQWidget() {
